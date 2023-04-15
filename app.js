2bfbacb12add5941d30adb367dc9df87ecd15cd0
irf009 <123625285+irf009@users.noreply.github.com>
2023-04-15 19:19:46 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const episodeNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 btn.addEventListener(`click`, function (e) {
   if (passWord.value.toUpperCase() === `MONK`) {
-    link.setAttribute("href", "https://www.dizigom.tv/dizi-izle/monk/");
+    link.setAttribute("href", "https://www.imdb.com/title/tt0312172/");
   }
   for (let i = 0; i < seasonNumbers.length; i++) {
     for (let b = 0; b < episodeNumbers.length; b++) {
@@ -17,7 +17,7 @@ btn.addEventListener(`click`, function (e) {
       ) {
         link.setAttribute(
           "href",
-          `https://www.dizigom.tv/monk-${seasonNumbers[i]}-sezon-${episodeNumbers[b]}-bolum-hd1/`
+          `https://watch.plex.tv/show/monk/season/${seasonNumbers[i]}/episode/${episodeNumbers[b]}`
         );
       }
     }
@@ -37,6 +37,12 @@ console.log(link.getAttribute("href"));
 
 btn.addEventListener("click", function adamGibiYaz() {
   if (link.getAttribute("href") === "#") {
-    alert("adam gibi yaz la");
+    alert(`Write => ${randomPassword[getRandomNumber()]}`);
   }
 });
+
+const randomPassword = [`monk`, `monk seasonNumber/episodeNumber`, `youtube`];
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 3);
+}
